d78a4bf7106d3e5c987cabd745e638df6dfb82cb
zahraahmadi2025 <zahraahmadi.43556@gmail.com>
2025-03-28 06:48:09 +0430

    file js

---
+++ public/dashbourd.js
@@ -0,0 +1,6 @@
+const bars =document.getElementById("bars");
+const horizontalNav =document.getElementById("hnav");
+const verticalNav =document.getElementById("vnav");
+bars.addEventListener("click",()=>{
+
+})
