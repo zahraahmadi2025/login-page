9c4978a572f446934b92fec402060ce7b281e964
zahraahmadi2025 <zahraahmadi.43556@gmail.com>
2025-03-28 07:07:48 +0430

    js

--- public/dashbourd.js
+++ public/dashbourd.js
@@ -2,5 +2,7 @@ const bars =document.getElementById("bars");
 const horizontalNav =document.getElementById("hnav");
 const verticalNav =document.getElementById("vnav");
 bars.addEventListener("click",()=>{
+    horizontalNav.classList.add("w-[75%]");
+    horizontalNav.classList.remove("w-full");
 
-})
+});
